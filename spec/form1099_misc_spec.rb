57c4d0419b23febcf93b241813b3ffe4f52ca655
Ryan Winograd <ryan@thewinograds.com>
2013-01-09 10:50:07 -0600

    Add spec for all copies of f1099msc

--- spec/form1099_misc_spec.rb
+++ spec/form1099_misc_spec.rb
@@ -72,3 +72,38 @@ describe "Form1099Misc Copy A" do
   end
 
 end
+
+describe "Form1099" do
+  before(:all) do
+    @form1099 = IrsForms::Form1099Misc.new(data: [
+      {:payer_contact_information => [
+        'Payer Name, LLC', 'Address Line 1',
+        'City, State 00000', '(555) 555-5555'],
+      :payer_federal_id => 'XX-XXXXXXX',
+      :recipient_federal_id => 'YY-YYYYYY3',
+      :recipient_name => 'Recipient Name',
+      :recipient_street_address => 'Street Address Line 1',
+      :recipient_city_state_zip => 'City, State 11111',
+      :recipient_account_number => 'XXXXX',
+      :nonemployee_compensation => '9875383.39'},
+      {:payer_contact_information => [
+          'Payer Name, LLC', 'Address Line 1',
+          'City, State 00000', '(555) 555-5555'],
+        :payer_federal_id => 'XX-XXXXXXX',
+        :recipient_federal_id => 'YY-YYYYYY2',
+        :recipient_name => 'Recipient Name',
+        :recipient_street_address => 'Street Address Line 1',
+        :recipient_city_state_zip => 'City, State 11111',
+        :recipient_account_number => 'XXXXX',
+        :nonemployee_compensation => '999'}
+    ])
+  end
+  it "should print correctly for each copy" do
+    IrsForms::Form1099Misc::COPIES.each do |copy|
+      @form1099.copy = copy
+      pdf = @form1099.to_pdf(:with_template => true)
+      write_content_to_file("f1099msc#{copy}_data_and_template.pdf", pdf)
+      assert_data_matches_file_content("f1099msc#{copy}_data_and_template.pdf", pdf)
+    end
+  end
+end
